d391c89c6e3354108d339c9b17264a8a73cb869d
Rush Perera <104535750+Rush-Perera@users.noreply.github.com>
2023-07-30 13:25:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet,StatusBar, Text, View , Button, SafeAreaView, Pressable, Alert} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+function App() {
+
+  const [t, setT]= useState("Text");
+
+  const ui = (
+
+    <SafeAreaView style={styles.container}>
+
       <StatusBar style="auto" />
-    </View>
+
+      <Text>{t}</Text>
+
+      <Button title="Click me" onPress={test} />
+
+      <Pressable onPress={test} onLongPress={longTest} onPressOut={pressout} delayLongPress={5000}>
+        <View style={styles.btnView}>
+          <Text style={styles.btnText}>Pressable</Text>
+        </View>
+      </Pressable>
+
+    </SafeAreaView>
+
   );
+
+  function test() {
+    Alert.alert('Info','Success');
+    setT("Text Changed");
+  }
+
+  return ui;
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +42,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  btnView: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    boxShadow: '0px 5px 5px rgba(0,0,0,0.5)',
+  },
+  
+
+btnText: {
+  color: 'white',
+  fontSize: 18,
+  fontWeight: 'bold',
+},
+
 });
+
+export default App;
+
+
+
+function longTest() {
+  Alert.alert('Info','Long Press Success');
+}
+
+function pressout() {
+  Alert.alert('Info','Press Out Success');
+}
